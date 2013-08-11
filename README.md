b5f8ab9f47ea6fedc014eaf717e6eba2c34acb0f
Andrey Popp <8mayday@gmail.com>
2013-08-11 23:41:43 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-dom-events
+
+Backbone.View-like DOM event delegation for React components. Use as a mixin for
+your components:
+
+    var DOMEvents = require('react-dom-events');
+    App = React.createClass({
+      mixins: [DOMEvents],
+      events: {
+        'click a': 'handleClickLink'
+      },
+      handleClickLink: function(e) {
+        e.preventDefault();
+        ...
+      },
+      ...
+    });
---
+++ index.js
@@ -0,0 +1,42 @@
+var delegateEventSplitter = /^(\S+)\s*(.*)$/;
+
+module.exports = {
+  delegateDOMEvents: function(events) {
+  	if (events === undefined) {
+			if (typeof this.events === 'function') {
+				events = this.events();
+			} else {
+				events = this.events;
+			}
+		}
+  	if (events === undefined) return;
+    this.undelegateDOMEvents();
+
+    var key, method;
+    for (key in events) {
+    	method = events[key];
+    	if (typeof method === 'function') {
+    		method = method.bind(this);
+			} else {
+				method = this[method];
+			}
+
+			if (!method) continue
+
+      var m = key.match(delegateEventSplitter);
+      var name = m[1], selector = m[2];
+      name += '.delegateEvents.' + this._rootNodeID;
+      var $el = window.$(this.getDOMNode())
+			selector === '' ?  $el.on(name, method) : $el.on(name, selector, method);
+		}
+	},
+  undelegateDOMEvents: function() {
+    window.$(this.getDOMNode()).off('.delegateEvents.' + this._rootNodeID);
+	},
+  componentDidMount: function() {
+    this.delegateDOMEvents();
+	},
+  componentWillUnmount: function() {
+    this.undelegateDOMEvents();
+	}
+}
